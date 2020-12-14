d68caa773bc15de850841a28bda49ebdb06da3d1
Jr Bytes <itamazor@gmail.com>
2020-12-14 14:24:17 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Strapi application
+
+A quick description of your strapi application
+
+
+## Using docker
+
+Run with docker compose file
+> docker-compose up -d
+
+Docker logs
+> docker-compose logs --tail=all -f
---
+++ config/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+module.exports = ({ env }) => ({
+  host: env('HOST', process.env.HOST),
+  port: env.int('PORT', process.env.PORT),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', process.env.ADMIN_JWT_SECRET),
+    },
+  },
+});
